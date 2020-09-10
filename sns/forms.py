7388344730413b158d398e86d3ff57bdac178fab
Katuhito Hara <katuhitohara@gmail.com>
2020-09-10 16:38:35 +0900

    model, form, view設定

---
+++ sns/forms.py
@@ -0,0 +1,62 @@
+from django import forms
+from .models import Message, Group, Friend, Good
+from django.contrib.auth.models import User
+
+# Messageのフォーム
+class MessageForm(forms.ModelForm):
+    class Meta:
+        model = Message
+        fields = ['owner', 'group', 'content']
+
+# Groupのフォーム
+class GroupForm(forms.ModelForm):
+    class Meta:
+        model = Group
+        fields = ['owner', 'title']
+
+# Friendsのフォーム
+class FriendForm(forms.ModelForm):
+    class Meta:
+        model = Friend
+        fields = ['owner', 'user', 'group']
+
+# Goodのフォーム
+class GoodForm(forms.ModelForm):
+    class Meta:
+        model = Good
+        fields = ['owner', 'message']
+
+# Groupの選択メニューフォーム
+class GroupSelectForm(forms.Form):
+    def __init__(self, user, *args, **kwargs):
+        super(GroupSelectForm, self).__init__(*args, **kwargs)
+        self.fields['groups'] = forms.ChoiceField(
+            choices = [('-', '-')] + [(item.title, item.title) for item in Group.objects.filter(owner=user],
+            widget=forms.Select(attrs={'class': 'form-control'}),
+        )
+
+# Friendのチェックボックスフォーム
+class FriendsForm(forms.Form):
+    def __init__(self, user, friends=[], vals=[], *args, **kwargs):
+        super(FriendsForm, self).__init__(*args, **kwargs)
+        self.fields['friends'] = forms.MultipleChoiceField(
+            choices = [(item.user, item.user) for item in friends],
+            widget=forms.CheckboxSelectMultiple(),
+            initial=vals
+        )
+
+# Group作成フォーム
+class CreateGroupFrom(forms.Form):
+    group_name = forms.CharField(max_length=50, widget=forms,TextInput(attrs={'class':'form-control'}))
+
+# 投稿フォーム
+class PostFrom(forms.Form):
+    content = forms.CharField(max_length=500, widget=forms,Textarea(attrs={'class':'form-control'. 'rows':2}))
+
+    def __init__(self, user, *args, **kwargs):
+        super(PostForm, self).__init__(*args, **kwargs)
+        public = User.objects.filter(username='public').first()
+        self.fields['groups'] = forms.ChoiceField(
+            choices = [('-', '-')] + [(item.title, item.title) for item in Group.objects.filter(owner__in=[user, public])],
+            widget=forms.Select(attrs={'class': 'form-control'}),
+        )
