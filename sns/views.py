2d7c441fb145bf07a6efa29475def9c513b29422
Katuhito Hara <katuhitohara@gmail.com>
2020-09-14 10:29:06 +0900

    second TEST

--- sns/views.py
+++ sns/views.py
@@ -122,7 +122,7 @@ def add(request):
     # 追加するUserを取得
     add_name = request.GET['name']
     add_user = User.objects.filter(username=add_name).first()
-    # Userが本人だった亜場合の処理
+    # Userが本人だった場合の処理
     if add_user == request.user:
         messages.info(request, "自分自身をFriendに追加することは出来ません。")
         return redirect(to='/sns')
@@ -167,7 +167,7 @@ def post(request):
         # Groupの取得
         group = Group.objects.filter(owner=request.user).filter(title=gr_name).first()
         if group == None:
-            (public_user, group) = get_public()
+            (pub_user, group) = get_public()
         # Messageを作成して保存
         msg = Message()
         msg.owner = request.user
